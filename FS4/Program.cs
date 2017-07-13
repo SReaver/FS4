a8797c62c7fb15a7225f82f8bbb22b513893d8f4
плотниковс <@403_4_>
2017-07-13 22:15:04 +0600

    DirEnumerating

--- FS4/Program.cs
+++ FS4/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FS4
 {
@@ -10,6 +11,49 @@ namespace FS4
     {
         static void Main(string[] args)
         {
+            DriveInfo[] di = DriveInfo.GetDrives();
+            
+            foreach (DriveInfo d in di)
+            {
+                Console.WriteLine(d.Name);
+                string t = d.Name.ToString().Substring(0, 1);
+                try
+                {
+                    DirectoryInfo dir = new DirectoryInfo(@"C:\Users\плотниковс\Desktop\" + t);
+                    dir.Create();
+                    FileInfo file = new FileInfo(dir.FullName);
+                    Console.WriteLine(dir.FullName);
+                    //FileStream fs = file.Create();
+                    //fs.Close();
+
+                    using (StreamWriter sw = new StreamWriter(@"C:\Users\плотниковс\Desktop\" + t+@"\123.txt"))
+                    {
+                        var txtText = "";
+                        DriveInfo diSelected = new DriveInfo(t);
+                        txtText = "Свободное пространство: " + diSelected.AvailableFreeSpace / 1024 / 1024 + "MB\n"
+                            + "Общий размер: " + diSelected.TotalSize + "\n"
+                            + "Формат устройства: " + diSelected.DriveFormat + "\n"
+                            + "Тип устройства: " + diSelected.DriveType + "\n"
+                            + "Готовность: " + diSelected.IsReady + "\n"
+                            + "Имя: " + diSelected.Name + "\n"
+                            + "Корневой каталог: " + diSelected.RootDirectory + "\n"
+                            + "Метка тома: " + diSelected.VolumeLabel;
+
+                        sw.Write(txtText);
+                        //Console.WriteLine(txtText);
+                    }
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+                }
+               
+                
+            }
+
+            
+            
         }
     }
 }
